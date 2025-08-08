3e146a4d7020bebe0b80bbf7651b303a87c8b8a7
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2025-08-08 16:27:15 -0300

    Adiciona estrutura inicial do projeto MVC
    Criado o controlador `Tarefas` no arquivo `HomeController1.cs`,
    herdando de `Controller` e com a ação `Index` que retorna uma
    `View`. Adicionado o modelo `Tarefa` no arquivo `Tarefa.cs`, com
    propriedades para ID, título, descrição e status de conclusão.
    Incluído o arquivo `Index.cshtml` como ponto de partida para a
    view associada à ação `Index` do controlador.

---
+++ AppTarefas/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppTarefas.Controllers
+{
+    public class Tarefas : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        // Nome da chave primaria deve ser Nome da Classse + "ID"
+        public int TarefaID { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
