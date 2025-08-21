849f07931d1f2f54d3426163d2a658a8cf05f9a0
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2025-08-21 11:52:01 -0300

    Adiciona CRUD para Tarefas e corrige nomenclatura
    Foi corrigido o nome da propriedade `TarefaID` para `TarefaId`
    na classe `Tarefa` e em todas as suas referências, garantindo
    consistência no padrão de nomenclatura.
    
    Adicionadas as ações `Edit`, `Details`, `Delete` e
    `DeleteConfirmed` no `TarefasController`, permitindo editar,
    visualizar detalhes e excluir tarefas.
    
    Atualizados os arquivos `Create.cshtml` e `Index.cshtml` para
    suportar o cadastro e exibição de tarefas. Adicionados os
    arquivos `Delete.cshtml`, `Details.cshtml` e `Edit.cshtml`
    para confirmação de exclusão, visualização de detalhes e
    edição de tarefas, respectivamente.

--- AppTarefas/Controllers/TarefasController.cs
+++ AppTarefas/Controllers/TarefasController.cs
@@ -27,11 +27,49 @@ namespace AppTarefas.Controllers
         {
             if (ModelState.IsValid)
             {
-                tarefa.TarefaID = _proximoId++;
+                tarefa.TarefaId = _proximoId++;
                 _tarefas.Add(tarefa);
                 return RedirectToAction("Index"); // Redireciona para a ação Index após criar a tarefa.
             }
             return View(tarefa);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(int id, Tarefa tarefaAtualizada)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            tarefa.Titulo = tarefaAtualizada.Titulo;
+            tarefa.Descricao = tarefaAtualizada.Descricao;
+            tarefa.Concluida = tarefaAtualizada.Concluida;
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefa != null)
+            {
+                _tarefas.Remove(tarefa);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- AppTarefas/Models/Tarefa.cs
+++ AppTarefas/Models/Tarefa.cs
@@ -3,7 +3,7 @@
     public class Tarefa
     {
         // Nome da chave primaria deve ser Nome da Classse + "ID"
-        public int TarefaID { get; set; }
+        public int TarefaId { get; set; }
         public string? Titulo { get; set; }
         public string? Descricao { get; set; }
         public bool Concluida { get; set; }
